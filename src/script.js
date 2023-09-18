287965d22a50c3143f8a591bd9ae1fa3ea0ea91e
Burrito-Princess <113419432+Burrito-Princess@users.noreply.github.com>
2023-09-19 00:14:37 +0200

    js tree

--- src/script.js
+++ src/script.js
@@ -10,4 +10,20 @@ function toggle() {
 
   function css_tail(){
     console.log("toggle");
+    let x = document.getElementById("css");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function js_tail(){
+    console.log("toggle");
+    let x = document.getElementById("js");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.html" // Include index.php
+    "./**/*.html" // Include index.html
   ],
   theme: {
     colors: {
@@ -12,8 +12,19 @@ module.exports = {
       'white': '#F2F2F2',
       'red': '#FF0000',
       'purple': '#A300FF',
+      'yellow': '#FFD500',
+      'green': '#00FF00',
     },
-    extend: {},
+    
+    extend: {
+      margin: {
+      'try': '2.7rem',
+      '15': '3.75rem',
+      '16': '4rem',
+      '17': '4.2rem',
+      '18': '4.5rem',
+      '22': '5.5rem',
+    },},
   },
   plugins: [],
 }
