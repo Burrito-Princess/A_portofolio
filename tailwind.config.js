b2d7bbfad4d648e9e432d462c8a8bdf6cb3f4bed
Burrito-Princess <willem.lol@outlook.com>
2023-09-19 14:53:11 +0200

    now use pos absolute for text (does it work?)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,27 +4,33 @@ module.exports = {
     "./**/*.html" // Include index.html
   ],
   theme: {
-    colors: {
-      'orange': '#FFA500',
-      'black': '#020202',
-      'accent_pink': '#F300DB',
-      'gray': '#727272',
-      'white': '#F2F2F2',
-      'red': '#FF0000',
-      'purple': '#A300FF',
-      'yellow': '#FFD500',
-      'green': '#00FF00',
-    },
-    
     extend: {
+      colors: {
+        'orange': '#FFA500',
+        'black': '#020202',
+        'accent_pink': '#F300DB',
+        'gray': '#727272',
+        'white': '#F2F2F2',
+        'red': '#FF0000',
+        'purple': '#A300FF',
+        'yellow': '#FFD500',
+        'green': '#00FF00',
+      },
       margin: {
-      'try': '2.7rem',
+      '12.5': '3.125rem',
+      '13': '3.25rem',
+      '13.5': '3.375rem',
       '15': '3.75rem',
       '16': '4rem',
       '17': '4.2rem',
       '18': '4.5rem',
+      '18.5': '4.6rem',
+      '19': '4.75rem',
       '22': '5.5rem',
-    },},
+    },
+    spacing: {
+      '90': '22.5rem',
+    },
   },
   plugins: [],
-}
+}}
